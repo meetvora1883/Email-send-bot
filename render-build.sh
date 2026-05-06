cccdcb7eef8fb19c99fee7cfbcdf357b239bc46d
meetvora1883 <meetvora1883@gmail.com>
2026-05-06 14:09:09 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # render-build.sh
-# Install Chromium for Puppeteer
-apt-get update && apt-get install -y chromium-browser
-# Install Node dependencies
+echo "Installing Node dependencies..."
 npm install
+
+echo "Installing Puppeteer's Chrome..."
+npx puppeteer browsers install chrome
+
+echo "Build complete."
